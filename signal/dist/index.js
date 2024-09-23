5efa90fb4dc29b1cd005ed4e18c1f81683af6dbb
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-09-24 05:13:10 +0530

    Add: Signal server

--- signal/dist/index.js
+++ signal/dist/index.js
@@ -3,12 +3,97 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@prisma/client");
 const express_1 = __importDefault(require("express"));
+const ws_1 = require("ws");
+const prisma = new client_1.PrismaClient();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.get("/api/v1/test", (req, res) => {
+const server = app.listen(8080, () => console.log("Server connected"));
+const wssChat = new ws_1.WebSocketServer({ noServer: true });
+const wssSignal = new ws_1.WebSocketServer({ noServer: true });
+// API endpoints
+app.get("/test", (_, res) => {
     res.status(200).json({
         message: "Working",
     });
 });
-app.listen(8080, () => console.log("Server connected"));
+app.post("/login", (req, res) => {
+    const userExist = prisma.user.findFirst({
+        where: {
+            email: req.body.email,
+            password: req.body.password
+        }
+    });
+    if (!userExist)
+        return res.status(403).json({
+            message: "Invalid username or password",
+        });
+    return res.status(200).json({
+        message: "Successful login",
+    });
+});
+// Chat Socket
+wssChat.on("connection", (ws) => {
+    console.log("Client connected");
+    ws.on("error", console.error);
+    ws.on("message", (data) => {
+        const message = JSON.parse(data.toString());
+        if (message.type !== "connect") {
+            wssChat.clients.forEach((client) => {
+                client.send(JSON.stringify(message));
+            });
+        }
+    });
+});
+// RTC Socket
+let senderSocket = null;
+let receiverSocket = null;
+wssSignal.on("connection", (ws) => {
+    console.log("Client connected");
+    ws.on("error", console.error);
+    ws.on("message", (data) => {
+        const message = JSON.parse(data);
+        if (message.type === "sender") {
+            senderSocket = ws;
+        }
+        else if (message.type === "receiver") {
+            receiverSocket = ws;
+        }
+        else if (message.type === "createOffer") {
+            if (ws !== senderSocket)
+                return;
+            receiverSocket.send(JSON.stringify({
+                type: "createOffer",
+                sdp: message.sdp,
+            }));
+        }
+        else if (message.type === "iceCandidate") {
+            if (ws === senderSocket) {
+            }
+            else if (ws === receiverSocket) {
+                senderSocket.send(JSON.stringify({
+                    type: "iceCandidate",
+                    candidate: message.candidate
+                }));
+            }
+        }
+    });
+});
+// Upgrade connections to web-socket
+server.on("upgrade", (req, socket, head) => {
+    const { pathname } = new URL(req.url, "http://locahost:8080");
+    if (pathname === "/chat") {
+        wssChat.handleUpgrade(req, socket, head, (ws) => {
+            wssChat.emit("connection", ws, req);
+        });
+    }
+    else if (pathname === "/rtc") {
+        wssSignal.handleUpgrade(req, socket, head, (ws) => {
+            wssSignal.emit("connection", ws, req);
+        });
+    }
+    else {
+        socket.destroy();
+    }
+});
--- signal/src/index.ts
+++ signal/src/index.ts
@@ -1,16 +1,101 @@
+import { PrismaClient } from "@prisma/client";
 import express, { Request, Response } from "express";
+import WebSocket, { WebSocketServer } from "ws";
+
+const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
+const server = app.listen(8080, () => console.log("Server connected"));
 
-app.get("/test", (req: Request, res: Response) => {
+const wssChat = new WebSocketServer({ noServer: true });
+const wssSignal = new WebSocketServer({ noServer: true })
+
+// API endpoints
+app.get("/test", (_: Request, res: Response) => {
   res.status(200).json({
     message: "Working",
   });
 });
 
 app.post("/login", (req: Request, res: Response) => {
-
+  const userExist = prisma.user.findFirst({
+    where: {
+      email: req.body.email,
+      password: req.body.password
+    }
+  });
+  if (!userExist) return res.status(403).json({
+    message: "Invalid username or password",
+  });
+  return res.status(200).json({
+    message: "Successful login",
+  });
 });
 
-app.listen(8080, () => console.log("Server connected"));
+// Chat Socket
+wssChat.on("connection", (ws) => {
+  console.log("Client connected");
+
+  ws.on("error", console.error);
+
+  ws.on("message", (data: any) => {
+    const message = JSON.parse(data.toString());
+    if (message.type !== "connect") {
+      wssChat.clients.forEach((client) => {
+        client.send(JSON.stringify(message));
+      });
+    }
+  });
+});
+
+// RTC Socket
+let senderSocket: null | WebSocket = null;
+let receiverSocket: null | WebSocket = null;
+
+wssSignal.on("connection", (ws) => {
+  console.log("Client connected");
+
+  ws.on("error", console.error);
+
+  ws.on("message", (data: any) => {
+    const message = JSON.parse(data);
+    if (message.type === "sender") {
+      senderSocket = ws;
+    } else if (message.type === "receiver") {
+      receiverSocket = ws;
+    } else if (message.type === "createOffer") {
+      if (ws !== senderSocket) return;
+      receiverSocket!.send(JSON.stringify({
+        type: "createOffer",
+        sdp: message.sdp,
+      }));
+    } else if (message.type === "iceCandidate") {
+      if (ws === senderSocket) {
+
+      } else if (ws === receiverSocket) {
+        senderSocket!.send(JSON.stringify({
+          type: "iceCandidate",
+          candidate: message.candidate
+        }));
+      }
+    }
+  });
+});
+
+// Upgrade connections to web-socket
+server.on("upgrade", (req: Request, socket, head) => {
+  const { pathname } = new URL(req.url as string, "http://locahost:8080");
+
+  if (pathname === "/chat") {
+    wssChat.handleUpgrade(req, socket, head, (ws) => {
+      wssChat.emit("connection", ws, req);
+    });
+  } else if (pathname === "/rtc") {
+    wssSignal.handleUpgrade(req, socket, head, (ws) => {
+      wssSignal.emit("connection", ws, req);
+    });
+  } else {
+    socket.destroy();
+  }
+});
